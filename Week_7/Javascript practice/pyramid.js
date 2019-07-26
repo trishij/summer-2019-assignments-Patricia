38dcf0ff87e42583d468bf0369fcfc6756bed6f5
trishij <pjgust@gmail.com>
2019-07-26 14:25:31 -0700

    added pyramid file

---
+++ Week_7/Javascript practice/pyramid.js
@@ -0,0 +1,37 @@
+function asterisk1() {
+    var 
+    
+    console.log("first asterisk");
+
+    asterisk1("*");
+
+
+}
+
+
+function asterisk2() {
+
+    asterisk2("**");s
+
+}
+
+function asterisk3() {
+
+    asterisk3("***");
+
+
+}
+
+function asterisk2() {
+
+    asterisk2("**");
+
+
+}
+
+function asterisk1() {
+
+    asterisk1("*");
+
+
+}
